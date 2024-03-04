1351a1b0fe6d8a5969092897eacd863657729c40
gianlucafalaschi <gian100gian@outlook.it>
2024-03-04 16:55:03 +0100

    feat: bonus completed

--- js/script.js
+++ js/script.js
@@ -6,24 +6,28 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23 */
 // AQUISIZIONE E PREPARAZIONE DATI
 // ci servono:
 // costante  
-const lastPasswordPart = "23";
-console.log(lastPasswordPart);
+const lastPasswordPart = 23;
+const lastPasswordPartAsNumber = parseInt(lastPasswordPart);
 //nome utente
 const userName = prompt("Qual'è il tuo nome?");
 console.log(userName);
 //cognome
 const userCognome = prompt("Qual'è il tuo cognome? ");
 console.log(userCognome);
-//colore preferitoGia
+//colore preferito
 const userFavColor = prompt("Qual'è il tuo colore preferito?");
 console.log(userFavColor);
+// numero dato dall'utente
+const userNumber = prompt("Dammi un numero");
+const userNumberasNumber = parseInt(userNumber);
+console.log(userNumberasNumber);
 // LOGICA DELLA MIA APPLICAZIONE
 //concateno nome + cognome + colore preferito + lastPasswordPart
 /* const passw = userName + userCognome + userFavColor + lastPasswordPart;
 console.log(passw); */
 
 //uso backticks
-const passw = `${userName}${userCognome}${userFavColor}${lastPasswordPart}`;
+const passw = `${userName}${userCognome}${userFavColor}${lastPasswordPartAsNumber + userNumberasNumber}`;
 
 // metto la costante password in lowercase
 const passwLow = passw.toLowerCase();
