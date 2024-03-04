1e221338b03a8e32ba904538c397830644272775
gianlucafalaschi <gian100gian@outlook.it>
2024-03-04 14:41:40 +0100

    feat: js comments and input

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23 */
+
+// AQUISIZIONE E PREPARAZIONE DATI
+// ci servono:
+// costante  
+const lastPasswordPart = '23';
+console.log(lastPasswordPart);
+//nome utente
+const userName = prompt("Qual'è il tuo nome?");
+console.log(userName);
+//cognome
+const userCognome = prompt("Qual'è il tuo cognome? ");
+console.log(userCognome);
+//colore preferitoGia
+const userFavColor = prompt("Qual'è il tuo colore preferito?");
+console.log(userFavColor);
+// LOGICA DELLA MIA APPLICAZIONE
+//concateno nome + cognome + colore preferito + lastPasswordPart
+
+
+
+//OUTPUT ALL'UTENTE
+
